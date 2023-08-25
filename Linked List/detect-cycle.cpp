9c083f8718dd5753d1fca198fa8f863afb205324
Bhushan-20 <bhushan72845@gmail.com>
2023-08-26 01:04:50 +0530

    Cycle Detection in a Singly Linked List

---
+++ Linked List/detect-cycle.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+class Node{
+    public:
+    int data ;
+    Node* next;
+
+    Node(int data){
+        this -> data =data;
+       this ->next=NULL;
+    }
+
+    
+
+
+};
+bool detectCycle(Node* head) {
+    if (head == nullptr || head->next == nullptr) {
+        return false;
+    }
+    std::map<Node*, bool> visited;
+    Node* temp = head;
+    while (temp != nullptr) {
+        if (visited[temp] == true) {
+            return true;
+        }
+        visited[temp] = true;
+        temp = temp->next;
+    }
+    return false;
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = head->next; 
+
+    bool hasCycle = detectCycle(head);
+    if (hasCycle) {
+        cout << "The linked list contains a cycle." << endl;
+    } else {
+        cout << "The linked list does not contain a cycle." <<endl;
+    }
+
+    // Clean up: Delete the linked list (don't forget to deallocate memory)
+    delete head->next->next->next; // Delete nodes in reverse order
+    delete head->next->next;
+    delete head->next;
+    delete head;
+
+    return 0;
+}
