24c213ceb18e1e3eb12de11e2c986e6d99c5a0ea
Bhushan-20 <bhushan72845@gmail.com>
2023-08-27 13:35:47 +0530

    Check if Linked List is Palindrome

--- Linked List/check-palindrome.cpp
+++ Linked List/check-palindrome.cpp
@@ -15,7 +15,7 @@ class ListNode {
         this->next = NULL;
     }
 };
-
+//approach 1
 bool checkpalindrome(vector<int> arr){
       int n=arr.size();
       int s=0;
@@ -39,6 +39,67 @@ bool checkpalindrome(vector<int> arr){
         }
         return checkpalindrome(arr);
 } 
+
+//Approach2
+/*ListNode* getMid(ListNode* head ) {
+        ListNode* slow = head;
+        ListNode* fast = head -> next;
+        
+        while(fast != NULL && fast-> next != NULL) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
+        }
+        
+        return slow;
+    }
+    ListNode* reverse(ListNode* head) {
+        
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        
+        while(curr != NULL) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(ListNode *head)
+    {
+        if(head -> next == NULL) {
+            return true;
+        }
+        
+        //step 1 -> find Middle
+        ListNode* middle = getMid(head);
+        //cout << "Middle " << middle->data << endl;
+        
+        //step2 -> reverse List after Middle
+        ListNode* temp = middle -> next;
+        middle -> next = reverse(temp);
+        
+        //step3 - Compare both halves
+        ListNode* head1 = head;
+        ListNode* head2 = middle -> next;
+        
+        while(head2 != NULL) {
+            if(head2->val != head1->val) {
+                return 0;
+            }
+            head1 = head1 -> next;
+            head2 = head2 -> next;
+        }
+    
+        //step4 - repeat step 2
+        temp = middle -> next;
+        middle -> next = reverse(temp);
+        
+        return true;
+    }*/
+
 
 
 
