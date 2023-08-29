e4861cb82fb378ac8fc45925b0f755191800cfa7
Bhushan-20 <bhushan72845@gmail.com>
2023-08-29 23:29:15 +0530

    3Sum Triplets in an array with zero sum

---
+++ Arrays/3sum.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+    if (nums.size() < 3) {
+        return {};
+    }
+    
+    vector<vector<int>> res;
+    sort(nums.begin(), nums.end());
+    
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) {
+            continue;
+        }
+        
+        int left = i + 1, right = nums.size() - 1;
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+            
+            if (sum == 0) {
+                res.push_back({nums[i], nums[left], nums[right]});
+                
+                while (left < right && nums[left] == nums[left + 1]) {
+                    left++;
+                }
+                while (left < right && nums[right] == nums[right - 1]) {
+                    right--;
+                }
+                
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+    
+    return res;
+}
+
+int main() {
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    
+    vector<vector<int>> result = threeSum(nums);
+    
+    cout << "Triplets with zero sum:" << endl;
+    for (const vector<int>& triplet : result) {
+        for (int num : triplet) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
